3e9e04c2f5d285df2941d704e8b6694d47fc20bb
orion <orinsion2000@gmail.com>
2019-12-15 17:24:59 +0200

    commit

--- mitten.py
+++ mitten.py
@@ -76,7 +76,7 @@ def accounts():
             account['balance'] = json.loads(r.content.decode())['balance']
         
         sd = user['accounts'][1:]
-        ret = '<p><font size="10"> Account ' + sd[0]['accountID'] + ': ' + str(sd[0]['balance']) + ' ' + sd[0]['currency'] + '</font></p> <br \>' + \
+        ret = '<p><font size="10"><b>Luca\'s accounts: </b> </font></p> <br /> <p><font size="10"> Account ' + sd[0]['accountID'] + ': ' + str(sd[0]['balance']) + ' ' + sd[0]['currency'] + '</font></p> <br \>' + \
         '<p><font size="10"> Account ' + sd[1]['accountID'] + ': ' + str(sd[1]['balance']) + ' ' + sd[1]['currency'] + '</font></p>'
 
         return ret
